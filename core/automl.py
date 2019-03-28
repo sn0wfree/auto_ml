29029eb1102ed8414318f5a6ae1fca639c9bb38b
sn0wfree <snowfreedom0815@gmail.com>
2019-03-28 19:55:07 +0800

    alter some

--- core/automl.py
+++ core/automl.py
@@ -19,16 +19,17 @@ def test(params):
 
 
 if __name__ == '__main__':
-    params_regressor = {'regressor': None, 'preprocessing': None, 'max_evals': 5,
+    params_regressor = {'regressor': None, 'preprocessing': None, 'max_evals': 15,
                         'trial_timeout': 100, 'seed': 1}
 
-    params_classifier = {'classifier': None, 'preprocessing': None, 'max_evals': 5,
+    params_classifier = {'classifier': None, 'preprocessing': None, 'max_evals': 15,
                          'trial_timeout': 100, 'seed': 1}
 
     # estimator = ModelBuilder.create_estimator(params_regressor)
-    modeldict = test(params_regressor)
-    files_ = 'test.model'
-    ModelStore._save(modeldict, files_)
-    modeldict2 = ModelStore._read(files_, protocol=2)
+    dataset_dict = test_dataset()
+    m = Models(params_classifier, dataset_dict)
 
-    print(modeldict, modeldict2)
+    print(m.fit_and_return(verbose_debug=False))
+
+
+
--- test/test_sample.py
+++ test/test_sample.py
@@ -93,6 +93,19 @@ def test_auto_ml(url='http://0.0.0.0:8279/auto_ml'):
 
 
 if __name__ == '__main__':
+    params_regressor = {'regressor': None, 'preprocessing': None, 'max_evals': 5,
+                        'trial_timeout': 100, 'seed': 1}
+
+    params_classifier = {'classifier': None, 'preprocessing': None, 'max_evals': 5,
+                         'trial_timeout': 100, 'seed': 1}
+
+    # estimator = ModelBuilder.create_estimator(params_regressor)
+    modeldict = test(params_regressor)
+    files_ = 'test.model'
+    ModelStore._save(modeldict, files_)
+    modeldict2 = ModelStore._read(files_, protocol=2)
+
+    print(modeldict, modeldict2)
     # from hyperopt import fmin, tpe, hp
     #
     # best = fmin(fn=lambda x: x ** 2,
