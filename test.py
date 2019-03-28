24b71d7823e7bc64f673b47d8bd58dc53eb6a458
sn0wfree <snowfreedom0815@gmail.com>
2019-03-28 19:09:54 +0800

    alter some and automatly update!

--- test.py
+++ test.py
@@ -3,25 +3,29 @@ import requests
 from parameter_parser import ModelStore
 
 
-def test_upload_file(files_='test.model'):
+def test_upload_file(url='http://0.0.0.0:8279/upload_file',
+                     files_='test.model'):
     strings = ModelStore._force_read(files_)
 
     # "text/plain"
     data = {'file': ('model.model', strings, "application/octet-stream")}
     # M2 = ModelStore._force_read_from_string(strings)
 
-    r = requests.post('http://0.0.0.0:8279/upload_file', files=data)
+    r = requests.post(url, files=data)
 
     print(r.text)
 
 
-def test_auto_ml():
-    params_regressor ={'regressor': 'Null', 'preprocessing': 'Null', 'max_evals': 5,
+def test_auto_ml(url='http://0.0.0.0:8279/auto_ml'):
+    import json
+    params_regressor = {'regressor': 'Null', 'preprocessing': '[]', 'max_evals': 5,
                         'trial_timeout': 100, 'seed': 1}
-    para = {'parameters': {'regressor': 'Null', 'preprocessing': 'Null', 'max_evals': 5,
-                        'trial_timeout': 100, 'seed': 1}}
-
-    r = requests.post('http://0.0.0.0:8279/auto_ml', files=para)
+    # {'regressor': 'Null', 'preprocessing': 'Null', 'max_evals': 5,
+    #  'trial_timeout': 100, 'seed': 1}
+    # para = {'parameters': ('parameter', json.dumps(params_regressor), 'application/json')}
+    print(json.dumps(params_regressor))
+    r = requests.post(url, params=params_regressor)
+    print(r.text)
 
 
 if __name__ == '__main__':
