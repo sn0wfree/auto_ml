712eab97b2f88a5278f0eb09003d71f64bd26bad
sn0wfree <snowfreedom0815@gmail.com>
2019-03-28 15:32:49 +0800

    add parameters set for collections

---
+++ parameterset.py
@@ -0,0 +1,26 @@
+# coding=utf8
+
+
+Regressors = ['svr', 'svr_linear', 'svr_rbf',
+              'svr_poly', 'svr_sigmoid',
+              'knn_regression', 'ada_boost_regression',
+              'gradient_boosting_regression', 'random_forest_regression', 'extra_trees_regression',
+              'sgd_regression', 'xgboost_regression']
+
+Classifiers = ['svc', 'svc_linear', 'svc_rbf', 'svc_poly',
+               'svc_sigmoid', 'liblinear_svc',
+               'knn', 'ada_boost', 'gradient_boosting',
+               'random_forest', 'extra_trees',
+               'decision_tree', 'sgd', 'xgboost_classification',
+               'multinomial_nb', 'gaussian_nb',
+               'passive_aggressive', 'linear_discriminant_analysis',
+               'quadratic_discriminant_analysis',
+               'one_vs_rest', 'one_vs_one', 'output_code']
+
+Preprocessing = ['pca', 'one_hot_encoder', 'standard_scaler',
+                 'min_max_scaler', 'normalizer', 'ts_lagselector', 'tfidf',
+                 'rbm', 'colkmeans']
+
+
+if __name__=='__main__':
+    pass
